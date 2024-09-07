0c2c5020baa4ba08915e76a159533fab49242ed3
yingtu35 <daniel860305@gmail.com>
2024-09-07 14:35:49 -0700

    change landing page layout

---
+++ app/constants/landing.ts
@@ -0,0 +1,7 @@
+export const LandingLogo = Object.freeze({
+  alt: "Gmail Cleaner",
+  width: 300,
+  height: 300,
+});
+
+export const LandingSubtitle = "The best app for cleaning up your Gmail inbox";
