5f75360341118151846a1b4f1e6a8451c061193f
tuying0917 <tuying@usc.edu>
2025-04-26 11:58:30 -0700

    fix: fix missing types

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -60,3 +60,20 @@ export type SchedulePromptType = {
 export type AIPromptType = z.infer<typeof PromptSchema>;
 
 export type AIFormValues = z.infer<typeof AIFormValuesSchema>
+
+export type TimeValue = {
+  comparison: "after" | "before";
+  value: Date;
+}
+
+export type AgeValue = {
+  comparison: "older than" | "newer than";
+  unit: "days" | "months" | "years";
+  value: number;
+}
+
+export type SizeValue = {
+  comparison: "greater than" | "less than";
+  unit: "Bytes" | "KB" | "MB";
+  value: number;
+}
