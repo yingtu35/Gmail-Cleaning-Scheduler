58c4d221706dc93b59ae8750e0a400ebd78b711a
tuying0917 <tuying@usc.edu>
2025-04-28 22:00:03 -0700

    refactor: extract detailGrid component

---
+++ components/task/info-detail.tsx
@@ -0,0 +1,5 @@
+import { DetailGrid } from "../DetailGrid";
+
+export function InfoDetail({ infoEntries }: { infoEntries: [string, any][] }) {
+  return <DetailGrid entries={infoEntries} maxDepth={3} />;
+}
