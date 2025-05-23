bf46d3fb5e1967bb872aaabe39bf21088029d2c0
tuying0917 <tuying@usc.edu>
2025-05-23 09:17:51 +0800

    fix: redirect user to landing page on signout

--- libs/actions.ts
+++ libs/actions.ts
@@ -24,7 +24,7 @@ export async function authenticate() {
 }
 
 export async function logOut() {
-  await signOut();
+  await signOut({ redirectTo: '/' });
 }
 
 export const getAuthenticatedUser = async () => {
