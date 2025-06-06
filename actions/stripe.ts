477682d6c3cfad403849757819eff4615ac9546d
tuying0917 <tuying@usc.edu>
2025-06-06 20:53:03 +0800

    feat: handle checkout session completed event

---
+++ actions/stripe.ts
@@ -0,0 +1,107 @@
+import Stripe from "stripe";
+
+import { stripe } from "@/libs/stripe/client";
+import log from "@/utils/log";
+import { NewSubscription, SubscriptionStatus } from "@/types/subscription";
+import { createSubscription, getMembershipTierByPriceId, getUserInfoByEmail } from "@/libs/actions";
+
+async function fullfillCheckoutOrder(lineItem: Stripe.LineItem, checkoutSession: Stripe.Checkout.Session) {
+  const { description: membershipTierName, price } = lineItem
+  const { 
+    customer_email, 
+    subscription: subscriptionId,
+    created: createdAt,
+  } = checkoutSession
+  if (!customer_email) {
+    log.error('No customer email found in checkout session', checkoutSession)
+    return false
+  }
+
+  if (!subscriptionId) {
+    log.error('No subscription found in checkout session', checkoutSession)
+    return false
+  }
+  if (typeof subscriptionId !== 'string') {
+    log.error('Subscription id is not a string', subscriptionId)
+    return false
+  }
+  if (!membershipTierName) {
+    log.error('No membership tier name found in line item', lineItem)
+    return false
+  }
+  if (!price) {
+    log.error('No price found in line item', lineItem)
+    return false
+  }
+
+  const { id: priceId } = price
+
+  // Get the user by email from the database
+  const user = await getUserInfoByEmail(customer_email)
+  if (!user) {
+    log.error('No user found in database', customer_email)
+    return false
+  }
+  // Get the membership tier by id from the database
+  const membershipTier = await getMembershipTierByPriceId(priceId)
+  if (!membershipTier) {
+    log.error('No membership tier price id found in database', priceId)
+    return false
+  }
+
+  const newSubscription: NewSubscription = {
+    subscriptionId: subscriptionId,
+    userId: user.id,
+    membershipTierId: membershipTier.id,
+    status: SubscriptionStatus.ACTIVE,
+    cancelAt: null,
+    canceledAt: null,
+    createdAt: new Date(createdAt),
+    updatedAt: new Date(createdAt),
+  }
+  
+  const subscription = await createSubscription(newSubscription)
+  if (!subscription) {
+    log.error('Failed to create subscription', newSubscription)
+    return false
+  }
+  
+  return true
+}
+
+export async function handleCompletedCheckoutSession(event: Stripe.CheckoutSessionCompletedEvent) {
+  const session: Stripe.Checkout.Session = event.data.object
+  const sessionId = session.id
+
+  try {
+    // Retrieve the Checkout Session from the API with line_items expanded
+    const checkoutSession = await stripe.checkout.sessions.retrieve(sessionId, {
+      expand: ['line_items'],
+    });
+
+    const { payment_status, line_items } = checkoutSession
+
+    if (!line_items) {
+      log.error('No line items found in checkout session', checkoutSession)
+      return false
+    }
+
+    if (payment_status === 'unpaid') {
+      log.error('Checkout session is unpaid', checkoutSession)
+      return false
+    }
+
+    const lineItem = line_items.data[0]
+    const isFulfilled = await fullfillCheckoutOrder(lineItem, checkoutSession)
+
+    if (!isFulfilled) {
+      log.error('Failed to fulfill order', { lineItem, checkoutSession })
+      return false
+    }
+
+    return true
+  } catch (err: any) {
+    log.error('Failed to retrieve checkout session', err)
+    return false
+  }
+}
--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -3,52 +3,8 @@ import Stripe from "stripe";
 
 import { stripe } from "@/libs/stripe/client";
 import log from "@/utils/log";
-
+import { handleCompletedCheckoutSession } from "@/actions/stripe";
 const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET!
-
-async function fulfillOrder(lineItem: Stripe.LineItem, checkoutSession: Stripe.Checkout.Session) {
-  // TODO: Implement the logic to fulfill the order
-  await new Promise(resolve => setTimeout(resolve, 1000))
-  log.info('Fulfilled order', { lineItem, checkoutSession })
-  return true
-}
-
-async function handleCompletedCheckoutSession(event: Stripe.CheckoutSessionCompletedEvent) {
-  const session: Stripe.Checkout.Session = event.data.object
-  const sessionId = session.id
-
-  try {
-    // Retrieve the Checkout Session from the API with line_items expanded
-    const checkoutSession = await stripe.checkout.sessions.retrieve(sessionId, {
-      expand: ['line_items'],
-    });
-
-    const { payment_status, line_items } = checkoutSession
-
-    if (!line_items) {
-      log.error('No line items found in checkout session', checkoutSession)
-      return false
-    }
-
-    if (payment_status === 'unpaid') {
-      log.error('Checkout session is unpaid', checkoutSession)
-      return false
-    }
-
-    const lineItem = line_items.data[0]
-    const isFulfilled = await fulfillOrder(lineItem, checkoutSession)
-
-    if (!isFulfilled) {
-      log.error('Failed to fulfill order', { lineItem, checkoutSession })
-      return false
-    }
-
-    return true
-  } catch (err: any) {
-    log.error('Failed to retrieve checkout session', err)
-    return false
-  }
-}
 
 async function handleUpdatedSubscription(event: Stripe.CustomerSubscriptionUpdatedEvent) {
   // TODO: Implement the logic to update the subscription
@@ -84,10 +40,10 @@ export async function POST(request: NextRequest) {
 
   switch (event.type) {
     case 'checkout.session.completed':
-      const savedSession = await handleCompletedCheckoutSession(event)
-      if (!savedSession) {
-        log.error('Failed to save completed checkout session', event)
-        return NextResponse.json({ error: 'Failed to save completed checkout session' }, { status: 500 })
+      const isSessionHandled = await handleCompletedCheckoutSession(event)
+      if (!isSessionHandled) {
+        log.error('Failed to handle completed checkout session', event)
+        return NextResponse.json({ error: 'Failed to handle completed checkout session' }, { status: 500 })
       }
       break
     case 'customer.subscription.created':
