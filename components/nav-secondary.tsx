5d84ed4e4f3cd7ce7f5f9cd144e6f8ccdbfb3c61
tuying0917 <tuying@usc.edu>
2025-07-01 17:39:17 +0800

    feat: show upgrade to pro button on sidebar

--- components/nav-secondary.tsx
+++ components/nav-secondary.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { LucideIcon } from "lucide-react"
 import Link from 'next/link';
+import { User } from "next-auth";
 
 import {
   SidebarGroup,
@@ -10,11 +11,17 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
 import { cn } from "@/utils/cn";
-
+import { SubscriptionDetails } from "@/types/subscription";
+import { MembershipTierName } from "@/types/membershipTier";
 export const NavSecondary = ({
+  user,
   items,
   ...props
 }: {
+  user: ({
+    id?: string;
+    subscriptionDetails?: SubscriptionDetails | null;
+  } & User) | null,
   items: {
     title: string
     href: string
@@ -22,25 +29,32 @@ export const NavSecondary = ({
     rightIcon?: LucideIcon
     target?: string
     rel?: string
+    isBasicOnly?: boolean
   }[]
 } & React.ComponentPropsWithoutRef<typeof SidebarGroup>) => {
+  const { subscriptionDetails } = user || {};
+  const { tierDetails } = subscriptionDetails || {};
+  const isBasicTier = tierDetails?.name === MembershipTierName.BASIC;
   return (
     <SidebarGroup {...props}>
       <SidebarGroupContent className="flex flex-col gap-2">
         <SidebarMenu>
-          {items.map((item) => (
-            <SidebarMenuItem key={item.title}>
-              <Link href={item.href} target={item.target || '_self'} rel={item.rel || undefined}>
-                <SidebarMenuButton className={cn('py-4', item.rightIcon && 'justify-between')}>
-                  <div className="flex items-center gap-2">
-                    <item.icon />
-                    {item.title}
-                  </div>
-                  {item.rightIcon && <item.rightIcon />}
-                </SidebarMenuButton>
-              </Link>
-            </SidebarMenuItem>
-          ))}
+          {items.map((item) => {
+            if (item.isBasicOnly && !isBasicTier) return null;
+            return (
+              <SidebarMenuItem key={item.title}>
+                <Link href={item.href} target={item.target || '_self'} rel={item.rel || undefined}>
+                  <SidebarMenuButton className={cn('py-4', item.rightIcon && 'justify-between')}>
+                    <div className="flex items-center gap-2">
+                      <item.icon />
+                      {item.title}
+                    </div>
+                    {item.rightIcon && <item.rightIcon />}
+                  </SidebarMenuButton>
+                </Link>
+              </SidebarMenuItem>
+            );
+          })}
         </SidebarMenu>
       </SidebarGroupContent>
     </SidebarGroup>
